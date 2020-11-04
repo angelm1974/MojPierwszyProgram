e595603d582a1444a539eca74672e8a1b4914197
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2020-11-04 13:29:31 +0100

    Hello World

--- MojPierwszyProgram/Program.cs
+++ MojPierwszyProgram/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MojPierwszyProgram
 {
@@ -10,6 +6,8 @@ namespace MojPierwszyProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
         }
     }
 }
