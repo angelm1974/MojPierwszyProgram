80579156fd7ee7f0b34385877b1561d8bba15bde
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2020-11-04 15:17:37 +0100

    Argument

--- MojPierwszyProgram/Program.cs
+++ MojPierwszyProgram/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 
+
 namespace MojPierwszyProgram
 {
     class Program
     {
+        //To jest mój komentarz
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            string imie = "Andrzej";
 
+            imie = args[0].ToString();
+             
+            // MojDrugiProgram.Program2.Kolo();
+            Console.WriteLine(imie);
+            Console.ReadLine();
         }
+
+        //static string imie()
+        //{
+        //    return "Adam";
+        //}
     }
 }
--- MojPierwszyProgram/Program3.cs
+++ MojPierwszyProgram/Program3.cs
@@ -1,1 +1,13 @@
+using System;
 
+namespace MojDrugiProgram
+{
+    class Program2
+    {
+     public static void Kolo()
+        {
+            Console.WriteLine("Ko³o");
+           // Console.ReadLine();
+        }
+    }
+}
